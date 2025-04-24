eba1589e42ba4f6a42196705bd9c360a491e6d3c
BryanRO-SpTech <bryan.grocha@sptech.school>
2025-04-24 13:19:35 -0300

    Update sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ LOCAL=$(git rev-parse main)
 REMOTE=$(git rev-parse origin/main)
 
 if [ "$LOCAL" != "$REMOTE" ]; then
-    echo "📦 Atualizando aplicação"
+    echo "Atualizando aplicação"
     git pull
 
     # Removendo imagem docker
@@ -14,7 +14,7 @@ if [ "$LOCAL" != "$REMOTE" ]; then
     docker rmi web-data-viz
 
     docker build -t web-data-viz .
-    docker run --name web-data-viz -p 80:80 --restart unless-stopped web-data-viz -d
+    docker run --name web-data-viz -p 80:80 --restart unless-stopped web-data-viz
 
     echo "Deploy do web-data-viz concluído"
 fi
