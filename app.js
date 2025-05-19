4781ea23e55e7f49e4950ee1738cb0bef47dbdf8
Guilherme <guilhermecamargo1978@gmail.com>
2025-05-19 12:15:01 -0300

    commit feito

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var empresaRouter = require("./src/routes/empresas");
 var cadastrarMaquinaRouter = require("./src/routes/server");
+var jiraRouter = require("./src/routes/jira.js")
 
 
 app.use(express.json());
@@ -31,6 +32,7 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/empresas", empresaRouter);
 app.use("/server", cadastrarMaquinaRouter);
+app.use("/jira", jiraRouter);
 
 
 app.listen(PORTA_APP, function () {
