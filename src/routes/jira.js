fb90e66d87e88d3e0396c491fe748a7652165265
Guilherme <guilhermecamargo1978@gmail.com>
2025-05-31 16:15:12 -0300

    Alteração 3.8

--- src/routes/jira.js
+++ src/routes/jira.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 const app = express();
 
-const email = 'ralph.latence.2025@gmail.com';
+const email = 'ana.latence.2025@gmail.com';
 const apiToken = process.env.TOKEN_ATLASSIAN;
 const domain = 'fernandolatence2025.atlassian.net';
 app.use(cors())
@@ -12,7 +12,7 @@ app.use(cors())
 const auth = Buffer.from(`${email}:${apiToken}`).toString('base64');
 
 router.get('/chamados-abertos', async (req, res) => {
-  const jql = 'project=KAN AND statusCategory!=Done'; // chave do projeto
+  const jql = 'project=KANBAN AND statusCategory!=Done'; // chave do projeto
 
   const response = await fetch(`https://${domain}/rest/api/3/search?jql=${encodeURIComponent(jql)}`, {
     method: 'GET',
