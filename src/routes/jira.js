cafcf61dc40118bed2c9b4287650d08edf5402f1
Mário Junior <mario.aaraujojr@sptech.school>
2025-06-05 12:38:37 -0300

    Update jira.js

--- src/routes/jira.js
+++ src/routes/jira.js
@@ -10,8 +10,8 @@ const auth = Buffer.from(`${email}:${apiToken}`).toString('base64');
 
 router.get('/chamados-abertos', async (req, res) => {
   const jql = 'project=KANBAN AND statusCategory != Done';
-
-  const response = await fetch(`https://${domain}/rest/api/3/search?jql=${encodeURIComponent(jql)}`, {
+  const maxResults = 100;
+  const response = await fetch(`https://${domain}/rest/api/3/search?jql=${encodeURIComponent(jql)}&maxResults=${maxResults}`, {
     method: 'GET',
     headers: {
       'Authorization': `Basic ${auth}`,
@@ -76,8 +76,8 @@ const abrirChamado = async (summary, description) => {
 
 router.get('/chamados-abertos-sem-atribuicao', async (req, res) => {
 const jql = 'project=KANBAN AND assignee=null AND statusCategory != Done';
-
-  const response = await fetch(`https://${domain}/rest/api/3/search?jql=${encodeURIComponent(jql)}`, {
+const maxResults = 100;
+  const response = await fetch(`https://${domain}/rest/api/3/search?jql=${encodeURIComponent(jql)}&maxResults=${maxResults}`, {
     method: 'GET',
     headers: {
       'Authorization': `Basic ${auth}`,
